8c68457732c7fde047e4b59a7fe880d5309e15e1
Conrado <conradoms@gmail.com>
2020-01-31 17:19:22 -0300

    Criação da API básica de crud

---
+++ CodigoAulaNode/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const users = ['Conrado', 'Simone', 'Miguel'];
+
+server.get('/users', (req, res) => {
+    res.json(users);
+})
+
+server.get('/users/:index', (req, res) => {
+    const { index } = req.params;
+    res.json(users[index]);
+});
+
+server.post('/users', (req, res) => {
+    const { nome } = req.body;
+    users.push(nome);
+    return res.json(users);
+});
+
+server.put('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const { nome } = req.body;
+
+    users[id] = nome;
+
+    return res.json(users);
+});
+
+server.delete('/users/:id', (req, res) => {
+    const { id } = req.params;
+    users.splice(id, 1);
+    return res.json(users);
+});
+
+server.listen(3000);
